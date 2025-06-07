54bc4767b529ab97d7a4a4011366949ad863df24
Macéo <maceogarnier@gmail.com>
2025-06-07 12:20:35 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function copierTexte(element) {
-    const texte = element.innerText;
-    navigator.clipboard.writeText(texte).then(() => {
-      alert("Texte copié !");
-    }).catch(err => {
-      console.error("Erreur lors de la copie :", err);
-    });
-  }
